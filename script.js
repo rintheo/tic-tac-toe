921dd9b7a6ffc619722015531f0911f26f6611d5
rintheo <107115767+rintheo@users.noreply.github.com>
2023-07-14 05:44:00 +0800

    Add function to play one round through the UI

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const game = (() => {
             for (let i = 0; i < _rows; i++) {
                 _board[i] = [];
                 for (let j = 0; j < _columns; j++) {
-                    _board[i].push('-');
+                    _board[i].push('');
                 }
             }
         }
@@ -20,10 +20,14 @@ const game = (() => {
         // Initialize the board for the first game
         resetBoard();
     
+        const getRowCount = () => _rows;
+        const getColumnCount = () => _columns;
         const getBoard = () => _board;
         const printBoard = () => console.log(_board); // For console version only
     
         return {
+            getRowCount,
+            getColumnCount,
             getBoard,
             printBoard, // For console version only
             resetBoard,
@@ -42,57 +46,57 @@ const game = (() => {
     ];
 
     let _activePlayer = _players[0];
-    const getActivePlayer = () => _activePlayer;
     const _switchActivePlayer = () => _activePlayer = _activePlayer === _players[0] ? _players[1] : _players[0];
-    const _printActivePlayer = () => console.log(`It's ${_activePlayer().name}'s turn!`);
+    const _printActivePlayer = () => console.log(`It's ${_activePlayer.name}'s turn!`);
 
     let _winningPlayer = '';
-    const getWinningPlayer = () => _winningPlayer;
 
     let _gameOver = false;
 
     const playRound = (row, column) => {
         if (_gameOver === true) return;
-        if (_gameBoard.getBoard()[row][column] !== '-') return;
+        if (_gameBoard.getBoard()[row][column] !== '') return;
 
-        _gameBoard.getBoard()[row][column] = _activePlayer().mark;
+        _gameBoard.getBoard()[row][column] = _activePlayer.mark;
         _gameBoard.printBoard(); // For console version only
+
+        // Return true for successful round
 
         if (_checkForWinner()) {
             _gameOver = true;
             console.log(`Winner is ${_winningPlayer.name}!`);
-            return;
+            return true;
         }      
                 
         if (_checkForDraw()) {
             _gameOver = true;
             console.log('draw');
-            return;
+            return true;
         }
 
         _switchActivePlayer();
         _printActivePlayer();
+
+        return true;
     }
     
     const _checkForWinner = () => {
-        const rows = _gameBoard.getBoard().length;
-        const columns = _gameBoard.getBoard()[0].length;
         let testLines = [];
         let testLineIndex = 0;
 
         // Log each row 
-        for (let i = 0; i < rows; i++) {
+        for (let i = 0; i < _gameBoard.getRowCount(); i++) {
             testLines[testLineIndex] = [];
-            for (let j = 0; j < columns; j++) {
+            for (let j = 0; j < _gameBoard.getColumnCount(); j++) {
                 testLines[testLineIndex].push(_gameBoard.getBoard()[i][j]);
             }
             testLineIndex++;
         }        
 
         // Log each column 
-        for (let j = 0; j < columns; j++) {
+        for (let j = 0; j < _gameBoard.getColumnCount(); j++) {
             testLines[testLineIndex] = [];
-            for (let i = 0; i < rows; i++) {
+            for (let i = 0; i < _gameBoard.getRowCount(); i++) {
                 testLines[testLineIndex].push(_gameBoard.getBoard()[i][j]);
             }
             testLineIndex++;
@@ -100,20 +104,20 @@ const game = (() => {
 
         // Log back diagonal
         testLines[testLineIndex] = [];
-        for (let i = 0; i <rows; i++) {
+        for (let i = 0; i <_gameBoard.getRowCount(); i++) {
             testLines[testLineIndex].push(_gameBoard.getBoard()[i][i]);
         }
         testLineIndex++;
 
         // Log front diagonal
         testLines[testLineIndex] = [];
-        for (let i = 0; i <rows; i++) {
-            testLines[testLineIndex].push(_gameBoard.getBoard()[i][columns - 1 - i]);
+        for (let i = 0; i <_gameBoard.getRowCount(); i++) {
+            testLines[testLineIndex].push(_gameBoard.getBoard()[i][_gameBoard.getColumnCount() - 1 - i]);
         }
 
         // Check logged testLines for winning condition
         for (testLine of testLines) {
-            if (testLine.every(cell => (cell !== '-') && (cell === testLine[0]))) {
+            if (testLine.every(cell => (cell !== '') && (cell === testLine[0]))) {
                 for (_player of _players) {
                     if (_player.mark === testLine[0]) {
                         _winningPlayer = _player;
@@ -125,21 +129,79 @@ const game = (() => {
     }
 
     const _checkForDraw = () => {
-        if (_gameBoard.getBoard().flat().every(cell => cell !== '-')) {
+        if (_gameBoard.getBoard().flat().every(cell => cell !== '')) {
             return true;
         }
-    };
+    }
 
     const resetGame = () => {
         _gameOver = false;
         _winningPlayer = '';
         _gameBoard.resetBoard();
-    };
+    }
+
+    const _screenController = (() => {
+        const mainTop = document.querySelector('.main-top>p');
+        const board = document.querySelector('.board');
+
+        const displayActivePlayer = () => {
+            mainTop.textContent = `${_activePlayer.name}'s turn`;
+        }
+
+        const displayWinningPlayer = () => {
+            mainTop.textContent = `${_winningPlayer.name} wins!!`;
+        }
+
+        const displayDraw = () => {
+            mainTop.textContent = `Draw!!`;
+        }
+
+        const clickBoard = (e) => {
+            if (playRound(e.target.dataset.row, e.target.dataset.column)) {
+                displayActivePlayer();
+                clearBoard();
+                generateBoard();
+                if (_gameOver) {
+                    (_winningPlayer !== '') ? displayWinningPlayer() : displayDraw() ;
+                }
+                // animateCell(e.target.dataset.row, e.target.dataset.column);
+            }
+        }
+
+        // const animateCell = (row, column) => {
+        //     const clickedCell = document.querySelector(`[data-row='${row}'][data-column='${column}']`)
+        //     clickedCell.classList.add('clicked');
+        // }
+
+        const clearBoard = () => {
+            while (board.firstChild) {board.removeChild(board.firstChild)};
+        }
+    
+        const generateBoard = () => {
+            for (let i = 0; i < _gameBoard.getRowCount(); i++) {
+                for (let j = 0; j < _gameBoard.getColumnCount(); j++) {
+                    const button = document.createElement('button');
+                    button.dataset.row = i;
+                    button.dataset.column = j;
+                    button.classList.add('cell');
+                    if (_gameBoard.getBoard()[i][j] === '') {
+                        _activePlayer.mark === 'o' ? button.classList.add('one') : button.classList.add('two');
+                    }
+                    button.textContent = _gameBoard.getBoard()[i][j];
+                    button.addEventListener('click', clickBoard);
+                    board.appendChild(button);
+                }           
+            }
+        }
+        
+        // Initialize for the first game
+        displayActivePlayer();
+        generateBoard();
+
+    })();
 
     return {
-        playRound,
+        playRound, // Can be removed later
         resetGame,
-        getWinningPlayer,
-        getActivePlayer,
     }
 })();
